f82417636339a51d5468fe9d37eaac21cbd38ac2
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-13 17:31:41 +0530

    setup express and db connection

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,20 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT =  process.env.PORT || 5000;
+console.log(process.env.PORT)
+app.use(express.json());
+
+//test
+
+app.get('/', (req,res)=>{
+    res.send("hello 22");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running port: ${PORT}`);
+})
