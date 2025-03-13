608d1f2e17ea7016971911cff6e4cec4731e5062
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-13 21:06:10 +0530

    jwt web token

--- back-end/server.js
+++ back-end/server.js
@@ -1,36 +1,71 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectTODB } from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectTODB } from "./config/db.js";
+import bcryptjs from "bcryptjs";
+import User from "./models/user.model.js";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
 const app = express();
 
-const PORT =  process.env.PORT || 5000;
-console.log(process.env.PORT)
+const PORT = process.env.PORT || 5000;
+console.log(process.env.PORT);
 app.use(express.json());
 
 //signup
 
-app.post("api/signup",async(req, res)=>{
+app.post("api/signup", async (req, res) => {
+  const { username, email, password } = req.body;
 
-    const{username, email,password} = req.body;
-
-    try {
-        if(!username || !password || !email){
-            throw new Error("All fields are required");
-        }
-
-        // const emailExists =
-    } catch (error) {
-
-        
-        
+  try {
+    if (!username || !password || !email) {
+      throw new Error("All fields are required");
     }
 
-})
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ message: "User already exit" });
+    }
 
-app.listen(PORT, async ()=>{
-    await connectTODB()
-    console.log(`Server running port: ${PORT}`);
-})
+    const usernameExits = await User.findOne({ username });
+    if (usernameExits) {
+      return res
+        .status(400)
+        .json({ message: "Username is taken, try another one" });
+    }
+
+    //hash password
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const userDoc = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    //JWT
+    if (userDoc) {
+      const token = jwt.sign({ id: userDoc._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 7*24*60*60*1000,
+      })
+    }
+
+    return res
+      .status(200)
+      .json({ user: userDoc, message: "User created successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.listen(PORT, async () => {
+  await connectTODB();
+  console.log(`Server running port: ${PORT}`);
+});
